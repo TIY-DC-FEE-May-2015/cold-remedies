2577baf8b1003f59d9ee57ce6ce9012a2cd954d9
Ghaea <ghaea.tam@gmail.com>
2015-06-16 10:49:57 -0400

    all remedies loading

--- public/index.js
+++ public/index.js
@@ -1,1 +1,32 @@
 /* Your JS code goes here! */
+
+$(document).on("ready", function() {
+
+	var coldRemedyFunction = Handlebars.compile( $("#coldRemediesTemplate").html() )
+
+	$.ajax({
+
+		url: "/remedy",
+		method: "GET",
+		data: {},
+		success: function(data) {
+
+			_.each(data, function(id) {
+
+				_.each(id, function(remedy) {
+					var htmlString = coldRemedyFunction(remedy)
+
+					console.log(htmlString)
+
+					$(".listOfRemedies").append(htmlString)
+				})
+				
+
+			})
+
+				
+		}
+
+	})
+
+})
