fed9ea1bc9a27d1349a9a84abc4928ab2fab5ee4
Ghaea <ghaea.tam@gmail.com>
2015-06-16 15:38:51 -0400

    toggleClass works globally, buttons don't work yet

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 /* Your JS code goes here! */
 
+var remedyArray = []
+
 $(document).on("ready", function() {
 
 	var coldRemedyFunction = Handlebars.compile( $("#coldRemediesTemplate").html() )
@@ -14,19 +16,63 @@ $(document).on("ready", function() {
 			_.each(data, function(id) {
 
 				_.each(id, function(remedy) {
-					var htmlString = coldRemedyFunction(remedy)
 
-					console.log(htmlString)
+					var remedyObj = {
+						name: remedy.name,
+						treatment: remedy.treatment,
+						prevention: remedy.prevention,
+						description: remedy.description,
+						reactions: remedy.reactions
+					}
+
+// Prevention value changes to text
+					if (remedyObj.prevention === 3) {
+						remedyObj.prevention = "Probably Effective"
+					}
+
+					if (remedyObj.prevention === 2) {
+						remedyObj.prevention = "Possibly Effective"
+					}
+
+					if (remedyObj.prevention === 1) {
+						remedyObj.prevention = "Unlikely to be Effective"
+					}
+
+					if (remedyObj.prevention === 0) {
+						remedyObj.prevention = "Ineffective"
+					}
+// Treatment value changes to text
+					if (remedyObj.treatment === 3) {
+						remedyObj.treatment = "Probably Effective"
+					}
+
+					if (remedyObj.treatment === 2) {
+						remedyObj.treatment = "Possibly Effective"
+					}
+
+					if (remedyObj.treatment === 1) {
+						remedyObj.treatment = "Unlikely to be Effective"
+					}
+
+					if (remedyObj.treatment === 0) {
+						remedyObj.treatment = "Ineffective"
+					}
+
+// Append to Template
+					var htmlString = coldRemedyFunction(remedyObj)
 
 					$(".listOfRemedies").append(htmlString)
+
 				})
 				
 
 			})
-
-				
+			
+			$(".name").on("click", function() {
+				$(".info").toggleClass("hidden")
+			})
 		}
-
+	
 	})
 
 })
