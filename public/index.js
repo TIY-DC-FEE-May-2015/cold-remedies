e06e672fa6e529f96f6be42295d0e2f93a3de9d7
Steve Koo <shkoo25@gmail.com>
2015-06-16 22:03:42 -0400

    Cold Remedy

--- public/index.js
+++ public/index.js
@@ -1,1 +1,73 @@
-/* Your JS code goes here! */
+
+
+var remedyList = function(callback){
+	
+	$.ajax({
+		url: "/remedy",
+		method:"GET",
+		success: function(data){
+		callback(data)
+		}
+
+	})
+
+}
+
+ var strength = function(i){
+ 	if (i === 3){
+ 		return "Probably effective"
+ 	}
+ 	
+ 	if (i === 2){
+ 		return "Possibly effective"
+ 	}
+
+ 	if (i === 1){
+		return "Unlikely to be effective"
+	}
+
+	if (i === 0){
+		return "Ineffective"
+	}
+
+ }
+
+
+var remedyStuff= function(data){
+
+	_.each(data.remedies, function(remedy){
+
+		var newRemedy = {
+			name: remedy.name,
+			prevention: strength(remedy.prevention),
+			treatment: strength(remedy.treatment),
+			description: remedy.description,
+			reaction: remedy.reaction
+		}
+
+
+		var htmlLeft = templateLeft(newRemedy)
+			$("#left-spot").append(htmlLeft)
+		})	
+
+		$(".leftNames").on("click", function(){
+			$(this).find(".description").toggleClass("active")
+			$(".description").css("color", "red")
+
+			console.log(this)
+		})
+
+
+
+
+}
+
+
+	$(document).on("ready", function(){
+		templateLeft = Handlebars.compile( $("#left-side").html() );
+
+
+		remedyList(remedyStuff)
+
+
+	})
