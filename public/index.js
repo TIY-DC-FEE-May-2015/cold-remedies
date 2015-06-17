4e6a8c9f3d6339d0b4b19fb266d181ec7fd8dc93
Tajaa Long <tajaalong@Tajaas-MacBook-Pro.local>
2015-06-17 19:06:39 -0400

    Maybe Updates?

--- public/index.js
+++ public/index.js
@@ -1,86 +1,85 @@
 /* Your JS code goes here! */
+
+var objRem
+
+	
+
 $(document).on("ready", function () {
-
-
-	//Lists the available cold remedies in the system 
-	//(requested via AJAX). 
-	var template = Handlebars.compile($("#rem").html() );
-
 	$.ajax({
-	      url:  "http://localhost:3000/remedy",
-	      method: "GET",
-	      data: {},
-	      success: function(data){
-	        console.log(data.remedies)	
+		  url: "http://localhost:3000/remedy",
+		  method: "GET",
+		  data: {},
+		  success: function(data){
+		        console.log(data.remedies)	
 
-	       _.each(data , function(id){
-	       	  _.each(id, function(remedy){
+		       _.each(data , function(id){
+		       	  _.each(id, function(remedy){
 
-	       	  	var remedyObj = {
-	       	  		name: remedy.name,
-	       	  		treatment: remedy.treatment,
-	       	  		prevention: remedy.prevention,
-	       	  		description: remedy.description,
-	       	  		reactions: remedy.reactions
-	       	  	}
+		       	  	var remedyObj = {
+		       	  		name: remedy.name,
+		       	  		treatment: remedy.treatment,
+		       	  		prevention: remedy.prevention,
+		       	  		description: remedy.description,
+		       	  		reactions: remedy.reactions
+					}
 
-	       	  	if(remedyObj.treatment === 0){
-			  	remedyObj.treatment = "ineffective" 
-			  }
+				 objRem = remedyObj;
+		         console.log(objRem) 
+				 var template = Handlebars.compile($("#rem").html() );
 
-			   if(remedyObj.treatment === 1){
-			  	remedyObj.treatment = "unlikely to be effective"
-			  }
+				 if(objRem.treatment === 0){
+				  	objRem.treatment = "ineffective"
+				  }
 
-			   if(remedyObj.treatment === 2){
-			  	remedyObj.treatment = "possibly effective"
-			  }
+				if(objRem.treatment === 1){
+				  	objRem.treatment = "unlikely to be effective"
+				  	
+				  }
 
-			   if(remedyObj.treatment === 3){
-			  	remedyObj.treatment = "probably effective"
-			  }
-			   
-			  if(remedyObj.prevention === 0){
-			  	remedyObj.prevention = "ineffective"
-			  }
+				   if(objRem.treatment === 2){
+				  objRem.treatment = "possibly effective"
+				  }
 
-			  if(remedyObj.prevention === 1){
-			  	remedyObj.prevention = "unlikely to be effective"
-			  }
+				   if(objRem.treatment === 3){
+				  	objRem.treatment = "probably effective"
+				  }
+				   
+				  if(objRem.prevention === 0){
+				  	objRem.prevention = "ineffective"
+				  }
 
-			  if(remedyObj.prevention === 2){
-			  	remedyObj.prevention = "possibly effective"
-			  }
-			  if(remedyObj.prevention === 3){
-			  	remedyObj.prevention = "probably effective"
-			  }
+				  if(objRem.prevention === 1){
+				  	objRem.prevention = "unlikely to be effective"
+				  }
 
-	       	  	 var htmlString = template(remedyObj)
-	        	$("#main-content").append(htmlString)
+				  if(objRem.prevention === 2){
+				  	objRem.prevention = "possibly effective"
+				  }
+				  if(objRem.prevention === 3){
+				  	objRem.prevention = "probably effective"
+				  } 
+
+				 var htmlString = template(objRem)
+				 $("#main-content").append(htmlString)       
+				})
+			  })
+
+		    $(".all").on("click", function (){
+    		$(this).find(".other").slideToggle(".hidden")
+    	})
+
+			}
+
+		})
+   
+	  
+	
 
 
+	
+	
 
-	       	  })
-				 
-	      	}) 	
-			   $(".all").on("click", function (){
-		    		$(this).find(".other").slideToggle(".hidden")
-		    	})
+	
 
-			  
-
-		}
-	       
-
-	   })
-	   
-	   
-
- 
-	//Displays the name of the remedy 
-	//and some visual indicator of its effectiveness at 
-	//treatment and prevention-- not the numbers 0-3 but some wording and/or color indicator.
-
- 
-
+	
   })
